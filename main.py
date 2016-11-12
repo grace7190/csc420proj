62fe2695cc94da47ce6df842151219677bb8fb81
grace7190 <grace.zhu@mail.utoronto.ca>
2016-11-12 14:36:22 -0500

    initial

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 14:31:49 2016
+
+@author: zhuyiyan, bilodea4
+"""
+
+
+if __name__ == '__main__':
+    # you apparently don't like to use this and it bugs the crap outta me :P
