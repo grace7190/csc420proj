9ada1466d16b2f5ba2bb343db6677b7fccccf90e
grace7190 <grace.zhu@mail.utoronto.ca>
2016-11-23 17:56:00 -0500

    put in video

--- main.py
+++ main.py
@@ -4,7 +4,44 @@ Created on Sat Nov 12 14:31:49 2016
 
 @author: zhuyiyan, bilodea4
 """
+import numpy as np
+import cv2
+#
+#Detect shots in the videos. A shot is a set of consecutive frames with a smooth camera
+#motion.
+#4. (Manually) Annotate shot boundaries in the video. How would you evaluate how well
+#you are detecting the shots? Compute your performance
+
 
 
 if __name__ == '__main__':
-    # you apparently don't like to use this and it bugs the crap outta me :P
+    # Define VideoCapture objects
+    cap_in = cv2.VideoCapture('/test.avi')
+    #cap_out = cv2.VideoCapture(0)
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.cv.CV_FOURCC(*'XVID')
+    out = cv2.VideoWriter('/output.xvid',fourcc, 20.0, (640,480))
+
+    while(cap_in.isOpened()):
+        # read frame
+        ret, frame = cap_in.read()
+        if ret == True:
+            # process frame
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            #cv2.imshow('frame',gray)
+            #cv2.waitKey(0.25)
+
+            # write frame
+            out.write(frame)
+            cv2.imshow('frame',frame)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+        
+    cap_in.release()
+    #cap_out.release()
+    out.release()
+    cv2.destroyAllWindows()
+    
